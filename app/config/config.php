4e3a999311c1d526658ee9637354402e6f1580ae
333736 <90184972+NijsvanDuin@users.noreply.github.com>
2023-01-23 10:01:38 +0100

    Internal Commit

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'BE-toets-MVC');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://www.mvc-oop-pdo-toets-userstory.com');
+
+define('SITENAME', 'MVC Framework Toets');
